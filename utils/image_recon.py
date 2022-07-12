cf90b4bbd4f2a6d8ac0c0e3c82965c2abfbf6981
KrakenLeaf <orensolo@gmail.com>
2022-07-12 12:09:28 -0500

    Update image_recon.py

--- utils/image_recon.py
+++ utils/image_recon.py
@@ -6,7 +6,10 @@ import os
 import torch
 import nibabel as nib
 import numpy as np
-
+from torch.fft import fft, ifft
+from torch.fft import fftshift
+from torch.fft import ifftshift
+    
 def image_sos(kspace, hdr=None, filesavename=[]):
     '''
     Reconstruct a Sum Of Squares (SOS) image from k-space data
@@ -15,15 +18,12 @@ def image_sos(kspace, hdr=None, filesavename=[]):
     :param filesavename: Optional path to save the reconstructed image as a NIfTI file (default is empty list)
     :return: Reconstructed SOS image
     '''
-    from torch.fft import fft
-    from torch.fft import fftshift
-    from torch.fft import ifftshift
     
     # Transform to image domain
     dims = [0, 2, 3]
     image = kspace
     for dim in dims:
-        image = ifftshift(fft(fftshift(image, dim=dim), dim=dim), dim=dim)
+        image = fftshift(ifft(ifftshift(image, dim=dim), dim=dim), dim=dim)
 
     # Combine sum of squares
     image = torch.squeeze(torch.sum(torch.square(torch.abs(image)), dim=1))
@@ -46,10 +46,6 @@ def image_fba(kspace, p=1, hdr=None, filesavename=[]):
     :param filesavename: Optional path to save the reconstructed image as a NIfTI file (default is empty list)
     :return: Reconstructed FBA image
     '''
-    from torch.fft import fft
-    from torch.fft import fftshift
-    from torch.fft import ifftshift
-
     # Permute such that coils dimension is first
     print("kspace dims {}".format(kspace.shape))
     kspace = torch.permute(kspace, dims=(1, 0, 2, 3))
@@ -70,7 +66,7 @@ def image_fba(kspace, p=1, hdr=None, filesavename=[]):
     # Transform to image domain
     dims = [0, 1, 2]
     for dim in dims:
-        image = ifftshift(fft(fftshift(image, dim=dim), dim=dim), dim=dim)
+        image = fftshift(ifft(ifftshift(image, dim=dim), dim=dim), dim=dim)
     image = torch.abs(image) # Remove residual imaginary parts
 
     if isinstance(filesavename, str):
